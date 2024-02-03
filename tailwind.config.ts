ee45acd5b9585f43aaa9f0c1aba32eb77dabf12f
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-02-03 17:23:32 +0900

    Update tailwind.config.ts and tsconfig.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import animatePlugin from 'tailwindcss-animate'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config = {
@@ -73,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animatePlugin],
 } satisfies Config
 
 export default config
