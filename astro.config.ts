77ede487e0e9cdff0c72b7f9bc58acd6be4cc4a1
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-02-10 14:40:37 +0900

    Refacto astro.config.ts with import

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import { HOMEPAGE_URL } from './config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://www.remimeullemeestre.dev'
-    : 'http://localhost:4321',
+  site: HOMEPAGE_URL,
   integrations: [
     react(),
     tailwind({
