ee7676ec8b1c8636af4bed064b7018928f66713f
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-02-15 14:19:57 +0900

    Add Grogd for type safelty and validation - 2nd draft with recommendation of groqd faq

--- frontend/src/lib/project.ts
+++ frontend/src/lib/project.ts
@@ -9,31 +9,42 @@ import {
   nullToUndefined,
 } from 'groqd'
 
-export const projectQuery = q('*')
-  .filter("_type == 'project' && defined(slug)")
-  .order('createdAt desc')
-  .grab({
-    slug: ['slug.current', q.string()],
-    mainImage: sanityImage('mainImage', { withCrop: true }).nullable(),
-    title: q.string(),
-    body: q('body')
-      .filter()
-      .select({
-        '_type == "block"': ['{...}', q.contentBlock()],
-        '_type == "figure"': {
-          _type: q.literal('figure'),
-          asset: q('asset').grabOne('_ref', q.string()),
-        },
-        default: {
-          _key: q.string(),
-          _type: ['"unsupported"', q.literal('unsupported')],
-          unsupportedType: ['_type', q.string()],
-        },
-      }),
-    skills: q('skills').filter().deref().grab({ title: q.string() }),
-    publishedAt: ['_createdAt', q.string()],
-    excerpt: q.string().nullable(),
-  })
+// export const projectQuery = q('*')
+//   .filter("_type == 'project' && defined(slug)")
+//   .order('createdAt desc')
+//   .grab({
+//     slug: ['slug.current', q.string()],
+//     mainImage: sanityImage('mainImage', { withCrop: true }).nullable(),
+//     title: q.string(),
+//     body: q('body')
+//       .filter()
+//       .select({
+//         '_type == "block"': ['{...}', q.contentBlock()],
+//         '_type == "figure"': {
+//           _type: q.literal('figure'),
+//           asset: q('asset').grabOne('_ref', q.string()),
+//         },
+//         default: {
+//           _key: q.string(),
+//           _type: ['"unsupported"', q.literal('unsupported')],
+//           unsupportedType: ['_type', q.string()],
+//         },
+//       }),
+//     skills: q('skills').filter().deref().grab({ title: q.string() }),
+//     publishedAt: ['_createdAt', q.string()],
+//     excerpt: q.string().nullable(),
+//   })
+
+export const pageProjectSelection = {
+  slug: ['slug.current', q.string()],
+  mainImage: sanityImage('mainImage', { withCrop: true }).nullable(),
+  title: q.string(),
+  skills: q('skills').filter().deref().grab({ title: q.string() }),
+  excerpt: q.string().nullable(),
+  createdAt: ['createdAt', q.string()],
+} satisfies Selection
+
+export type PageProjectsProps = TypeFromSelection<typeof pageProjectSelection>
 
 export const runQuery = makeSafeQueryRunner(
   (query, params: Record<string, unknown> = {}) =>
