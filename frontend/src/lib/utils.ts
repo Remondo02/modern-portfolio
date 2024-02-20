e4a9cdec059b33e648a246561a7b18f4a2230c6d
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-02-20 15:32:13 +0900

    Change page title, description, add a function to slice description on single post

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import {type Props} from '@/pages/project/[slug].astro'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function metaDescriptionSlice(text: Props[]) {
+  return text
+    .map((block) => {
+      if (block._type !== 'block' || !block.children) {
+        return ''
+      }
+      return block.children.map((child: any) => child.text).join('')
+    })
+    .join('\n\n')
+    .slice(0, 157)
+    .concat('...')
 }
